61e23f6336ff55b294a5e0e2d5512fe1cc55eed4
GabeDavidson <gdavidson@ma.org>
2018-01-17 08:29:39 -0800

    test of the test

---
+++ Unit1_strings/Reverse String.py
@@ -0,0 +1,13 @@
+def reverse(s):
+    r_string = ""
+
+    for x in range(len(s)):
+        r_string += s[len(s) - x - 1]
+
+    return r_string
+
+astring = input("Input string: ")
+
+reverse_string = reverse(astring)
+
+print(reverse_string)
